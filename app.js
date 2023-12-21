38f2342a1d602de391cba8cfee7993584a3b7645
Kweebac <06.amarshall@gmail.com>
2023-12-21 21:23:08 +0000

    Added login & membership functionality

--- app.js
+++ app.js
@@ -2,12 +2,59 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const indexRouter = require("./controllers/index");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const User = require("./models/user");
+const bcrypt = require("bcrypt");
 
 mongoose.connect(process.env.MONGO_PRIVATE_URL);
 
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email }).exec();
+
+      if (!user) return done(null, false, { message: "No user with that email" });
+      if (!(await bcrypt.compare(password, user.password)))
+        return done(null, false, { message: "Incorrect password" });
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id).exec();
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 const app = express();
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(`${__dirname}/public`));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 app.use("/", indexRouter);
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
 
 app.listen(3000);
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,20 @@ const router = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const { body, validationResult } = require("express-validator");
+const passport = require("passport");
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) next();
+  else res.redirect("/login");
+}
+function checkNotAuthenticated(req, res, next) {
+  if (!req.isAuthenticated()) next();
+  else res.redirect("/");
+}
 
 router.get("/", (req, res) => res.render("index"));
-router.get("/register", (req, res) =>
+
+router.get("/register", checkNotAuthenticated, (req, res) =>
   res.render("register", {
     user: undefined,
     errors: [],
@@ -38,10 +49,57 @@ router.post("/register", [
     const errors = validationResult(req);
     if (errors.isEmpty()) {
       await user.save();
-      res.redirect("/");
+      res.redirect("/login");
     } else {
       res.render("register", {
         user,
+        errors: errors.array(),
+      });
+    }
+  },
+]);
+
+router.get("/login", checkNotAuthenticated, (req, res) =>
+  res.render("login", {
+    user: undefined,
+    errors: [],
+  })
+);
+router.post("/login", [
+  body("email").escape().trim(),
+  body("password").escape().trim(),
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+]);
+
+router.get(
+  "/membership",
+  checkAuthenticated,
+  (req, res, next) => {
+    if (req.user.membership) res.redirect("/");
+    else next();
+  },
+  (req, res) => res.render("membership", { secretCode: undefined, errors: [] })
+);
+router.post("/membership", [
+  body("secretCode")
+    .escape()
+    .trim()
+    .custom((value, { req }) => {
+      return value === "CyberedCake";
+    })
+    .withMessage("Secret code is incorrect"),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      await User.findOneAndUpdate({ email: req.user.email }, { membership: true });
+      res.redirect("/");
+    } else {
+      res.render("membership", {
+        secretCode: req.body.secretCode,
         errors: errors.array(),
       });
     }
